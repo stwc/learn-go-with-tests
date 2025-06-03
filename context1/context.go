7705154e5f42bd9ac80776e4592782dc94b2676b
stwc <22775405+stwc@users.noreply.github.com>
2025-06-03 17:00:18 +0800

    Context

---
+++ context1/context.go
@@ -0,0 +1,24 @@
+package context1
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			// log error message
+			return
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
