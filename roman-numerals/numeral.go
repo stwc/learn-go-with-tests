68a1ee3cf2aaebd8536472f2f8e79db7c385581f
stwc <22775405+stwc@users.noreply.github.com>
2025-06-12 10:12:20 +0800

    Intro to property based tests

---
+++ roman-numerals/numeral.go
@@ -0,0 +1,52 @@
+package romannumerals
+
+import "strings"
+
+// ConvertToArabic converts a Roman Numeral to an Arabic number.
+func ConvertToArabic(roman string) uint16 {
+	var arabic uint16 = 0
+
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+
+	return arabic
+}
+
+// ConvertToRoman converts an Arabic number to a Roman Numeral.
+func ConvertToRoman(arabic uint16) string {
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
+
+type romanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+var allRomanNumerals = []romanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
